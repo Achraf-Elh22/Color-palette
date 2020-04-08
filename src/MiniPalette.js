94743b63f4867892633927a23951ba1beb45f0be
achraf elhamzaoui <achrafelh22@gmail.com>
2020-04-08 09:12:12 +0100

    start styling MiniPalette Comp

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import {withStyles} from "@material-ui/styles";
+import { withStyles } from '@material-ui/styles';
 
 const styles = {
-    main:{
-        backgroundColor:"purple",
-        border: "3px solid teal"
-    }
+  root: {
+    backgroundColor: 'white',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    border: '1px solid black',
+    overflow: 'hidden',
+    '&:hover': {
+      cusror: 'pointer',
+    },
+  },
+  colors: { backgroundColor: 'grey' },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relatice',
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem',
+  },
+};
+
+function MiniPalette(props) {
+  const { classes, paletteName, emoji } = props;
+  return (
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
+    </div>
+  );
 }
 
-function MiniPalette(props){
-    const {classes} = props
-    console.log(classes)
-    return(
-        <div className={classes.main} >
-            <h1>Mini Palette</h1>
-        </div>
-    )
-}
-
-export default withStyles(styles)(MiniPalette)
+export default withStyles(styles)(MiniPalette);
