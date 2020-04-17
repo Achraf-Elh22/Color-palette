0731fc617e32221e7b42aa20998de0c1e0eeeb7f
achraf elhamzaoui <achrafelh22@gmail.com>
2020-04-17 11:40:46 +0100

    add SavePalatte functionality

--- src/App.js
+++ src/App.js
@@ -10,18 +10,36 @@ import SingleColorPalette from './SingleColorPalette';
 import NewPaletteForm from './NewPaletteForm';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      palettes: seedColors,
+    };
+    this.savePalette = this.savePalette.bind(this);
+    this.findPalette = this.findPalette.bind(this);
+  }
   findPalette(id) {
-    return seedColors.find((palette) => palette.id === id);
+    return this.state.palettes.find((palette) => palette.id === id);
+  }
+
+  savePalette(newPalette) {
+    this.setState({ palettes: [...this.state.palettes, newPalette] });
   }
   render() {
     return (
       <Switch>
-        <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
+        <Route
+          exact
+          path='/palette/new'
+          render={(routeProps) => (
+            <NewPaletteForm savePalette={this.savePalette} {...routeProps} />
+          )}
+        />
         <Route
           exact
           path='/'
           render={(routeProps) => (
-            <PaletteList palettes={seedColors} {...routeProps} />
+            <PaletteList palettes={this.state.palettes} {...routeProps} />
           )}
         />
         <Route
@@ -38,13 +56,16 @@ class App extends React.Component {
         <Route
           exact
           path='/palette/:paletteId/:colorId'
-          render={(routeProps) => <SingleColorPalette colorId={routeProps.match.params.colorId} palette={GeneratePalette(
-            this.findPalette(routeProps.match.params.paletteId)
-          )} />}
+          render={(routeProps) => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={GeneratePalette(
+                this.findPalette(routeProps.match.params.paletteId)
+              )}
+            />
+          )}
         />
       </Switch>
-
-
     );
   }
 }
