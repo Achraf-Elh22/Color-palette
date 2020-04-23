f0899c822361c462a64432af3ba0b8cda1ece70b
achraf elhamzaoui <achrafelh22@gmail.com>
2020-04-23 10:20:49 +0000

    make ColorBox respnsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -16,11 +16,11 @@ export default {
 
     [sizes.down('lg')]: {
       width: '25%',
-      height: (props) => (props.showingFullPalette ? '20%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '20%' : '33.33333%'),
     },
     [sizes.down('md')]: {
       width: '50%',
-      height: (props) => (props.showingFullPalette ? '20%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '10%' : '20%'),
     },
     [sizes.down('xs')]: {
       width: '100%',
@@ -123,6 +123,7 @@ export default {
       padding: '1rem',
       textTransform: 'uppercase',
       marginBottom: '0',
+      [sizes.down('xs')]: { fontSize: '5rem' },
     },
     '& p': {
       fontSize: '2rem',
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './size';
+
 export default {
   Palette: {
     height: '100vh',
@@ -17,6 +19,18 @@ export default {
     marginBottom: '-4px',
     opacity: '1',
     backgroundColor: '#000',
+    [sizes.down('lg')]: {
+      width: '25%',
+      height: '33.3333%',
+    },
+    [sizes.down('md')]: {
+      width: '50%',
+      height: '20%',
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: '10%',
+    },
   },
 
   copyButton: {
