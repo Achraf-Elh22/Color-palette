d1f962e46fe6d4adb50a14f127ef5c00ae0c8147
achraf elhamzaoui <achrafelh22@gmail.com>
2020-04-25 14:50:49 +0000

    Fix navbar slider size and add classnames to ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import classNames from 'classnames';
 
 import styles from './styles/ColorBoxStyles';
 
@@ -34,11 +35,15 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`${classes.copyOverlay} ${
-              copied && classes.showOverlay
-            } `}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied,
+            })}
           />
-          <div className={`${classes.copyMsg} ${copied && classes.showMsg} `}>
+          <div
+            className={classNames(classes.copyMsg, {
+              [classes.showMsg]: copied,
+            })}
+          >
             <h1>Copied!</h1>
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
